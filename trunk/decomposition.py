cff12d7fbaf4734339dbd8ba54d12891828f8e82
mcdonough.colin@gmail.com <mcdonough.colin@gmail.com@617955be-e429-ff8c-0e58-096088924133>
2012-04-30 07:26:31 +0000

    Added Kernel and Sparse PCA

--- trunk/decomposition.py
+++ trunk/decomposition.py
@@ -1,13 +1,17 @@
 # Copyright 2012 Colin McDonough (cmcdonough@wustl.edu)
 
 from module2_GS import *
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import KernelPCA
 from sklearn.decomposition import PCA
+from sklearn.decomposition import SparsePCA
+from sklearn.decomposition import RandomizedPCA
 from sklearn.externals import joblib
 
 X = normalization('gene_IndividualsArr.pkl', 'top10Genes_Indiv.pkl')
 random_pca_data = normalization('gene_IndividualsArr.pkl', 'top10Genes_Indiv.pkl')
 pca_data = normalization('gene_IndividualsArr.pkl', 'top10Genes_Indiv.pkl')
+sparse_pca_data = normalization('gene_IndividualsArr.pkl', 'top10Genes_Indiv.pkl')
+kernel_pca_data = normalization('gene_IndividualsArr.pkl', 'top10Genes_Indiv.pkl')
 
 random_pca = RandomizedPCA(n_components=50)
 random_pca_model = random_pca.fit(random_pca_data)
@@ -26,3 +30,16 @@ print 'pca explained sum', sum(pca.explained_variance_ratio_)
 joblib.dump(pca_model, 'pca_model.pkl')
 joblib.dump(pca_X_new, 'pca_X_new.pkl')
 print pca_model
+
+sparse_pca = SparsePCA(n_components=50)
+sparse_pca_model = pca.fit(sparse_pca_data)
+sparse_pca_X_new = pca.fit_transform(X)
+joblib.dump(sparse_pca_model, 'sparse_pca_model.pkl')
+joblib.dump(sparse_pca_X_new, 'sparse_pca_X_new.pkl')
+print sparse_pca_model
+
+kernel_pca = KernelPCA(n_components=50)
+kernel_pca_model = kernel_pca.fit(kernel_pca_data)
+kernel_X_new = kernel_pca.fit_transform(X)
+joblib.dump(kernel_pca_model, 'kernel_pca_model.pkl')
+joblib.dump(kernel_X_new, 'kernel_X_new.pkl')
